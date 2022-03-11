f4d4aa90bb69f3485ac20f25d1c85d54b2ef0da6
Murat <mrdudov@gmail.com>
2022-03-11 13:53:40 +0300

    SELECT, DISTINCT, WHERE

--- postgresql/script.sql
+++ postgresql/script.sql
@@ -17,9 +17,10 @@ CREATE TABLE person (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(150),
     gender VARCHAR(7) NOT NULL,
     date_of_birth DATE NOT NULL,
-    email VARCHAR(150)
+    country_of_birth VARCHAR(50)
 );
 
 INSERT INTO person (
@@ -37,4 +38,24 @@ INSERT INTO person (
     date_of_birth,
     email
 )
-VALUES ('Jake', 'Jonse', 'MALE', date '1990-12-31', 'jake#gmail.com');
+VALUES ('Jake', 'Jonse', 'MALE', date '1990-12-31', 'jake@gmail.com');
+
+SELECT * from person;
+
+SELECT first_name, last_name FROM person;
+
+-- 123 - ASC
+-- 321 - DESC
+
+SELECT * FROM person ORDER BY first_name DESC, country_of_birth ASC;
+
+SELECT DISTINCT country_of_birth FROM person ORDER BY country_of_birth;
+
+SELECT * 
+FROM person 
+WHERE 
+    gender = 'Female' 
+    AND 
+        (country_of_birth = 'Poland' OR country_of_birth = 'China')
+    AND
+        last_name = 'Kim';
