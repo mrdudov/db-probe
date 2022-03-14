10bca136588dc4395b0c2d6e3e24b8ceede18895
Murat <mrdudov@gmail.com>
2022-03-14 22:23:11 +0300

    northwind nested querys

---
+++ postgresql/northwind/nested_query/nested_query.sql
@@ -0,0 +1,40 @@
+SELECT
+    company_name
+FROM
+    suppliers
+WHERE
+    country IN(
+        SELECT 
+            DISTINCT country
+        FROM 
+            customers
+    );
+
+
+SELECT 
+    DISTINCT suppliers.company_name
+FROM
+    suppliers
+JOIN 
+    customers USING(country);
+
+
+SELECT category_name, SUM(units_in_stock)
+FROM products
+INNER JOIN categories USING(category_id)
+GROUP BY category_name
+ORDER BY SUM(units_in_stock) DESC
+LIMIT (
+    SELECT MIN(product_id) + 4
+    FROM products
+);
+
+
+
+SELECT product_name, units_in_stock
+FROM products
+WHERE units_in_stock > (
+    SELECT AVG(units_in_stock)
+    FROM products
+)
+ORDER BY units_in_stock;
