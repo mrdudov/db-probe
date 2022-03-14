00bc4d0f445fc7e0ba02208fbc98541f65d095dc
Murat <mrdudov@gmail.com>
2022-03-14 19:29:51 +0300

    northwind AS

--- postgresql/northwind/inner_join.sql
+++ postgresql/northwind/inner_join.sql
@@ -105,3 +105,43 @@ FROM
     orders
 NATURAL JOIN -- never use NATURAL JOIN, this is bad practice
     employee;
+
+
+SELECT 
+    COUNT(*) AS employee_count
+FROM
+    employees;
+
+
+SELECT 
+    COUNT(DISTINCT country) AS country
+FROM
+    employees;
+
+
+SELECT 
+    category_id,
+    SUM(units_in_stock) AS units_in_stock
+FROM
+    products
+GROUP BY 
+    category_id
+ORDER BY
+    units_in_stock DESC
+LIMIT 
+    5;
+
+
+SELECT
+    category_id,
+    SUM(unit_price * units_in_stock) AS total_price
+FROM
+    products
+WHERE
+    discontinued <> 1
+GROUP BY 
+    category_id
+HAVING 
+    SUM(unit_price * units_in_stock) > 5000
+ORDER BY
+    total_price DESC;
