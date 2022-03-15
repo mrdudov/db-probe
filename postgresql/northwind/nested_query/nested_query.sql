2880ae413efa1c6eb44753035b2f226e4c42c106
Murat <mrdudov@gmail.com>
2022-03-15 11:01:46 +0300

    northwind ANY, ALL

--- postgresql/northwind/nested_query/nested_query.sql
+++ postgresql/northwind/nested_query/nested_query.sql
@@ -38,3 +38,41 @@ WHERE units_in_stock > (
     FROM products
 )
 ORDER BY units_in_stock;
+
+
+SELECT DISTINCT company_name
+FROM customers
+JOIN orders USING(customer_id)
+JOIN order_details USING(order_id)
+WHERE quantity > 40;
+
+
+SELECT DISTINCT company_name
+FROM customers
+WHERE customer_id = ANY(
+    SELECT customer_id
+    FROM orders
+    JOIN order_details USING(order_id)
+    WHERE quantity > 40
+);
+
+
+SELECT DISTINCT product_name, quantity
+FROM products
+JOIN order_details USING(product_id)
+WHERE quantity > (
+    SELECT AVG(quantity)
+    FROM order_details)
+ORDER BY quantity;
+
+
+
+
+SELECT DISTINCT product_name, quantity
+FROM products
+JOIN order_details USING(product_id)
+WHERE quantity > ALL(
+    SELECT AVG(quantity)
+    FROM order_details
+    GROUP BY product_id)
+ORDER BY quantity;
