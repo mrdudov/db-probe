ffaf2b511a6f68743a9003a31d1be19cfa5edecc
Murat <mrdudov@gmail.com>
2022-03-18 12:34:56 +0300

    northwind functions in, out, default

--- postgresql/northwind/functions/querys.sql
+++ postgresql/northwind/functions/querys.sql
@@ -40,3 +40,41 @@ CREATE OR REPLACE FUNCTION get_avg_price() RETURNS real AS $$
 $$ LANGUAGE SQL;
 
 SELECT get_avg_price() AS avg_price;
+
+
+-- in, out, default
+
+CREATE OR REPLACE FUNCTION get_product_price_by_name(IN prod_name varchar) RETURNS real AS $$
+    SELECT unit_price
+    FROM products
+    WHERE product_name = prod_name
+$$ LANGUAGE SQL;
+
+
+SELECT get_product_price_by_name('Chocolade') AS price;
+
+CREATE OR REPLACE FUNCTION get_price_boundaries(OUT max_price real, OUT min_price real) AS $$
+    SELECT MAX(unit_price), MIN(unit_price)
+    FROM products
+$$ LANGUAGE SQL;
+
+SELECT get_price_boundaries();
+
+SELECT * 
+FROM get_price_boundaries();
+
+
+CREATE OR REPLACE FUNCTION get_price_boundaries_by_discontinuity(IN is_discontinued int DEFAULT 1, OUT max_price real, OUT min_price real) AS $$
+    SELECT MAX(unit_price), MIN(unit_price)
+    FROM products
+    WHERE discontinued = is_discontinued
+$$ LANGUAGE SQL;
+
+SELECT get_price_boundaries_by_discontinuity();
+
+SELECT * 
+FROM get_price_boundaries_by_discontinuity();
+
+SELECT * 
+FROM get_price_boundaries_by_discontinuity(0);
+
