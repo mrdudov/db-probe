47a9df4c15056cfe0b91356cdf2e9314c2a6f102
Murat <mrdudov@gmail.com>
2022-03-18 11:56:03 +0300

    northwind scalar functions

--- postgresql/northwind/functions/querys.sql
+++ postgresql/northwind/functions/querys.sql
@@ -25,3 +25,18 @@ SELECT fix_customers_region();
 SELECT *
 FROM tmp_customers;
 
+-- scalar functions 
+
+CREATE OR REPLACE FUNCTION get_total_number_of_goods() RETURNS bigint AS $$
+    SELECT SUM(units_in_stock)
+    FROM products;
+$$ LANGUAGE SQL;
+
+SELECT get_total_number_of_goods();
+
+CREATE OR REPLACE FUNCTION get_avg_price() RETURNS real AS $$
+    SELECT avg(unit_price)
+    FROM products;
+$$ LANGUAGE SQL;
+
+SELECT get_avg_price() AS avg_price;
