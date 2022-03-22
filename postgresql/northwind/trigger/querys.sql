96c1dbdb34d1125e2c9cf5d20cc443c793234ff1
Murat <mrdudov@gmail.com>
2022-03-22 23:40:00 +0300

    northwind triggers audit

--- postgresql/northwind/trigger/querys.sql
+++ postgresql/northwind/trigger/querys.sql
@@ -99,3 +99,83 @@ VALUES
 SELECT *
 FROM employees
 WHERE employee_id = 10;
+
+
+DROP TABLE IF EXISTS products_audit;
+
+CREATE TABLE products_audit
+(
+    op char(1) NOT NULL,
+    user_changed text NOT NULL,
+    time_stamp timestamp NOT NULL,
+
+	product_id smallserial NOT NULL,
+	product_name varchar(40) NOT NULL,
+	supplier_id int2 NULL,
+	category_id int2 NULL,
+	quantity_per_unit varchar(20) NULL,
+	unit_price float4 NULL,
+	units_in_stock int2 NULL,
+	units_on_order int2 NULL,
+	reorder_level int2 NULL,
+	discontinued int4 NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION build_audit_products() RETURNS trigger AS $$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        INSERT INTO products_audit
+        SELECT 'I', session_user, NOW(), nt.* FROM new_table nt;
+    ELSEIF TG_OP = 'UPDATE' THEN 
+        INSERT INTO products_audit
+        SELECT 'U', session_user, NOW(), nt.* FROM new_table nt;
+    ELSEIF TG_OP = 'DELETE' THEN 
+        INSERT INTO products_audit
+        SELECT 'D', session_user, NOW(), ot.* FROM old_table ot;
+    END IF;
+    RETURN NULL;
+END
+$$ LANGUAGE plpgsql;
+
+
+DROP TRIGGER IF EXISTS audit_products_insert ON products;
+CREATE TRIGGER audit_products_insert AFTER INSERT ON products
+REFERENCING NEW TABLE AS new_table
+FOR EACH STATEMENT EXECUTE PROCEDURE build_audit_products();
+
+DROP TRIGGER IF EXISTS audit_products_update ON products;
+CREATE TRIGGER audit_products_update AFTER UPDATE ON products
+REFERENCING NEW TABLE AS new_table
+FOR EACH STATEMENT EXECUTE PROCEDURE build_audit_products();
+
+DROP TRIGGER IF EXISTS audit_products_delete ON products;
+CREATE TRIGGER audit_products_delete AFTER DELETE ON products
+REFERENCING OLD TABLE AS old_table
+FOR EACH STATEMENT EXECUTE PROCEDURE build_audit_products();
+
+
+SELECT *
+FROM products
+ORDER BY product_id DESC;
+
+INSERT INTO products 
+(
+    product_name,supplier_id,category_id,quantity_per_unit,unit_price,
+    units_in_stock,units_on_order,reorder_level,discontinued
+) 
+VALUES
+('Konbu',6,8,'2 kg box',6.0,24,0,5,0);
+
+SELECT * FROM products_audit;
+
+UPDATE products
+SET unit_price = 60
+WHERE product_id = 79;
+
+SELECT * FROM products_audit;
+
+DELETE FROM products
+WHERE product_id = 79;
+
+SELECT * FROM products_audit;
+
